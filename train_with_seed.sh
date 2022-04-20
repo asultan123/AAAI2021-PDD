70c41e81f65307452bba63370390650505b72cfc
asultan123 <asultan@aucegypt.edu>
2022-04-20 03:02:10 -0400

    will handle model saving based on seed in train script

--- train_with_seed.sh
+++ train_with_seed.sh
@@ -9,10 +9,10 @@
 
 seed=$1
 nvidia-smi
-model_dir=/scratch/sultan.a/AAAI2021-PDD/model_$seed
+model_dir=/scratch/sultan.a/AAAI2021-PDD/adp_model/model_$seed
 
 sleep 10;
 echo Training with $seed
-# python train_adp.py --model ensemble_3_resnet18 --seed $seed --model-basepath $model_dir --dataset CIFAR100 --bs 64 --lr 0.02 --alpha 2.0 --lamda 0.5 --save_dir bs
+python train_adp.py --model ensemble_3_resnet18 --seed $seed --model-basepath $model_dir --dataset CIFAR100 --bs 64 --lr 0.02 --alpha 2.0 --lamda 0.5 --save_dir bs
 
 exit;
