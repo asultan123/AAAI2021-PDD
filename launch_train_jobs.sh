474b4fc2ba89d98cbd948aedc7cb66a1f318efe0
asultan123 <asultan@aucegypt.edu>
2022-04-20 02:58:32 -0400

    updated launch scripts

--- launch_train_jobs.sh
+++ launch_train_jobs.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# pyenv shell PD
+eval "$(pyenv init -)"
+
+pyenv shell PD
+
+REPORTS_PATH=slurm_reports/adp_train
 
 function train_seeds()
 {
     seeds=(8388 1759 5933 7916 7445 6130 7422 4066 3098 5469 4456 2302 9062 2724 8420);
+    seed=${seeds[0]};
+    rm -rf $REPORTS_PATH/seed_${seeds[0]};
+    mkdir -p $REPORTS_PATH/seed_${seeds[0]};
     echo launching job with seed ${seeds[0]};
-    last_job_id=$(sbatch --dependency=afterany: train_with_seed.sh $seed);
+    last_job_id=$(sbatch --parsable --output=$REPORTS_PATH/seed_$seed/stdout --error=$REPORTS_PATH/seed_$seed/stderr train_with_seed.sh $seed);
 
     for seed in ${seeds[@]:1}; do
-        echo launching job with seed $seed
-	    $(sbatch --dependency=afterany:$last_job_id train_with_seed.sh $seed)
+        echo launching job with seed $seed;
+        rm -rf $REPORTS_PATH/seed_$seed;
+        mkdir -p $REPORTS_PATH/seed_$seed;
+	new_job_id=$(sbatch --parsable --output=$REPORTS_PATH/seed_$seed/stdout --error=$REPORTS_PATH/seed_$seed/stderr --dependency=afterany:$last_job_id train_with_seed.sh $seed);
+        last_job_id=$new_job_id;
     done
 }
 
--- train_with_seed.sh
+++ train_with_seed.sh
@@ -4,18 +4,14 @@
 #SBATCH --job-name=seeds_train
 #SBATCH --mem=8GB
 #SBATCH --ntasks=8
-#SBATCH --gres=ce-mri
-#SBATCH --output=./slurm_reports/train_seeds.%j.out
-#SBATCH --error=./slurm_reports/train_seeds.%j.err
-#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1 
+#SBATCH --partition=ce-mri
 
+seed=$1
 nvidia-smi
 model_dir=/scratch/sultan.a/AAAI2021-PDD/model_$seed
 
-which python
-
-seed=$1
-
+sleep 10;
 echo Training with $seed
 # python train_adp.py --model ensemble_3_resnet18 --seed $seed --model-basepath $model_dir --dataset CIFAR100 --bs 64 --lr 0.02 --alpha 2.0 --lamda 0.5 --save_dir bs
 
